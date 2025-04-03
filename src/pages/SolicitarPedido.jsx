24cdb7b6abac83721f6310f501ac4e936feaa34b
Ernesto Vicencio Mas <masoneweernesto@gmail.com>
2025-04-03 09:16:13 +0800

    actualizando

--- src/pages/SolicitarPedido.jsx
+++ src/pages/SolicitarPedido.jsx
@@ -102,8 +102,6 @@ function SolicitarPedido() {
   return (
     <div style={styles.container}>
       {errorMessage && <p style={styles.errorText}>{errorMessage}</p>}
-      <h1>{webViewData ? webViewData.id : "No hay"}</h1>
-      <h1>{webViewData?.notificacion ? "true" : "false"}</h1>
       <form onSubmit={enviarSolicitud} style={styles.form}>
         <textarea
           placeholder="Descripción del Pedido"
